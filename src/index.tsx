6aa4bcd612340f619dcbeafec95d3e5aa234c5f1
Hironori Ichimiya <hiron@hironytic.com>
2018-12-19 22:30:15 +0900

    Use Firebase SDK auto-configuration

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,21 @@ import "firebase/firestore";
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import firebaseConfig from './FirebaseConfig';
 import './index.css';
 import { unregister as unregisterServiceWorker } from './registerServiceWorker';
 
+async function getFirebaseConfig(): Promise<object> {
+  if (process.env.NODE_ENV !== 'production') {
+    const config = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG!)
+    return config;
+  } else {
+    return (await fetch('/__/firebase/init.json')).json();
+  }
+}
+
 async function initialize() {
   // Initialize Firebase
-  firebase.initializeApp(await firebaseConfig());
+  firebase.initializeApp(await getFirebaseConfig());
 
   // Initialize Cloud Firestore through Firebase
   const db = firebase.firestore();
