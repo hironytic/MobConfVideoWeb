0ec7b8be9855ac44250174e2bd184b70231cf547
Hironori Ichimiya <hiron@hironytic.com>
2022-06-29 21:12:18 +0900

    Install MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AppBar, CssBaseline, Toolbar, Typography } from '@mui/material';
 
-function App() {
+export function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="sticky">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">MobConfVideo</Typography>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 }
-
-export default App;
