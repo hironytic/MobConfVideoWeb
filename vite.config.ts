c9d2b3db34b1e5b5ec05d56138f6c53068d57469
Hironori Ichimiya <hiron@hironytic.com>
2023-09-28 19:36:20 +0900

    Add removed svgr again

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import svgrPlugin from 'vite-plugin-svgr';
+import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
@@ -13,5 +13,6 @@ export default defineConfig({
   plugins: [
     viteTsconfigPaths(),
     react(),
+    svgr(),
   ],
 });
