17a4f357f64bfe0f16c8b75e0083c30a19c88c71
Hironori Ichimiya <hiron@hironytic.com>
2023-11-05 15:26:48 +0900

    Update icon on TweetButton
    - Change the icon to X from lovely bird
    - Change the word to "ポスト" (Post) from "ツイート" (Tweet)

--- src/utils/TweetButton.tsx
+++ src/utils/TweetButton.tsx
@@ -22,8 +22,7 @@
 // THE SOFTWARE.
 //
 
-import { Button } from "@mui/material"
-import { Twitter } from "@mui/icons-material"
+import { Button, createSvgIcon } from "@mui/material"
 
 interface TweetButtonProps {
   text?: string
@@ -45,12 +44,19 @@ export function createTweetUrl({ text, url, hashtags }: TweetButtonProps): strin
   return tweetUrl.toString()
 }
 
+const XLogo = createSvgIcon(
+  <svg viewBox="-200 -250 1400 1750" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" fill="currentColor"/>
+  </svg>,
+  "X"
+)
+
 export function TweetButton(props: TweetButtonProps): JSX.Element {
   const tweetUrlString = createTweetUrl(props)
   
   return (
     <Button href={tweetUrlString} target="_blank" rel="noopener noreferrer" color="primary">
-      <Twitter/> ツイート
+      <XLogo/> ポスト
     </Button>
   )
 }
